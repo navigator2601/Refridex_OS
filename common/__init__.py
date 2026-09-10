33a4b6c0bc6d9e87c248e79824be057b3740d252
navigator2601 <navigator.vip14@gmail.com>
2026-09-10 11:20:51 +0300

    Очищений проект для розробки з нуля.

--- common/__init__.py
+++
@@ -1,5 +0,0 @@
-# common/__init__.py
-
-from .constants import ACCESS_LEVEL_BUTTONS, ALL_MENU_BUTTONS, BUTTONS_PER_PAGE
-from .messages import get_access_level_description, get_random_admin_welcome_message
-from .states import MenuStates, AdminStates # <--- ДОДАНО ІМПОРТ AdminStates та MenuStates
--- middlewares/__init__.py
+++
@@ -1,4 +0,0 @@
-# middlewares/__init__.py
-
-from .db_middleware import DbSessionMiddleware
-from .telethon_middleware import TelethonClientMiddleware # Додано
